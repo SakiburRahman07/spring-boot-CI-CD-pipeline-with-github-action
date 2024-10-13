809245e3c7c014240d18268dd562038eb88f42c4
sakib <sakiburrahman11.msr@gmail.com>
2024-10-13 16:12:50 +0600

    first commit

---
+++ src/main/java/com/sakib/githubAutomation/GithubAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.sakib.githubAutomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sakib/githubAutomation/GithubAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakib.githubAutomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
