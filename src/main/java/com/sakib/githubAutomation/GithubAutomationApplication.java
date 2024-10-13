dbcbdc6a8be7b2ebc8bcea3db24da271f42ba448
sakib <sakiburrahman11.msr@gmail.com>
2024-10-13 16:28:38 +0600

    action

--- src/main/java/com/sakib/githubAutomation/GithubAutomationApplication.java
+++ src/main/java/com/sakib/githubAutomation/GithubAutomationApplication.java
@@ -2,9 +2,18 @@ package com.sakib.githubAutomation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubAutomationApplication {
+
+		@GetMapping("/welcome")
+		public String welcome()
+		{
+			return "Welcome to teligati";
+		}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubAutomationApplication.class, args);
